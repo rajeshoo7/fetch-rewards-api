fa0838babc67dd020947fabc5898aba3781d066b
rajeshoo7 <rajeshsai.kammari@gmail.com>
2021-09-01 16:05:07 -0500

    FR-0001:FR-0001 Initiated Repo

---
+++ api-exercise/src/test/java/com/fetch/apiexercise/ApiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fetch.apiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
